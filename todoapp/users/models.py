35c861dd0034860b7ee72688b1b85a2283f6b32a
Rohit Pandey <rohit.pandey@joshtechnologygroup.com>
2026-02-14 14:20:54 +0530

    Superuser Creation Successfull

--- todoapp/users/models.py
+++ todoapp/users/models.py
@@ -4,14 +4,32 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
     """
     Add manager methods here to create user and super user
     """
+    def create_user(self, email, password, **extra_fields):
+        if not email : 
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
 
-
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
@@ -25,12 +43,17 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     - date_joined (default should be time of object creation)
     - last_name (max_length=150)
     """
-    is_superuser = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     first_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    is_staff = models.BooleanField()
-    date_joined = models.DateTimeField()
+    email = models.EmailField(_("email address"),unique=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
     last_name = models.CharField(max_length=150)
+
     USERNAME_FIELD = 'email'
+
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
